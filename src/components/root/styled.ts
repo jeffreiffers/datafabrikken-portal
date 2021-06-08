b2f4651a294bf683cff913b248679a4c13bf61d7
Øyvind Isaksen <oyvindisaks1@gmail.com>
2021-06-08 14:42:01 +0200

    fix(root): minor style

--- src/components/root/styled.ts
+++ src/components/root/styled.ts
@@ -4,10 +4,12 @@ import { Colour, theme } from '../../entrypoints/main/app/theme';
 
 const Root = styled.main`
   background-color: ${theme.colour(Colour.NEUTRAL, 'N60')};
+  color: ${theme.colour(Colour.NEUTRAL, 'N0')};
   display: flex;
   flex-direction: column;
   flex: 1;
   height: auto;
+  word-break: break-word;
 `;
 
 export default { Root };
