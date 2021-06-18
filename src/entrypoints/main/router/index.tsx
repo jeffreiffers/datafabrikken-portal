528dcbb95fd0d9849b88348d1cbc652b21a4939f
Øyvind Isaksen <oyvindisaks1@gmail.com>
2021-06-18 11:07:46 +0200

    fix(router): change order and add exact

--- src/entrypoints/main/router/index.tsx
+++ src/entrypoints/main/router/index.tsx
@@ -9,9 +9,9 @@ const isFeatureToggleActive = localStorage.getItem('DF_TOGGLE');
 
 const routes = {
   [PATHNAME.MAIN]: lazy(() => import('./pages/main-page')),
-  [PATHNAME.DATASETS]: lazy(() => import('./pages/datasets-page')),
   [PATHNAME.ABOUT]: lazy(() => import('./pages/article-page')),
   [PATHNAME.FIND_DATA]: lazy(() => import('./pages/find-data-page')),
+  [PATHNAME.DATASETS]: lazy(() => import('./pages/datasets-page')),
   [PATHNAME.NEWS]: lazy(() => import('./pages/news-page')),
   NEWS_ARTICLE: lazy(() => import('./pages/news-article-page'))
 };
@@ -22,21 +22,23 @@ const Router: FC = () => (
     <Suspense fallback={null}>
       <Switch>
         <Route exact path={PATHNAME.MAIN} component={routes[PATHNAME.MAIN]} />
+        <Route exact path={PATHNAME.ABOUT} component={routes[PATHNAME.ABOUT]} />
         <Route
-          path={`${PATHNAME.FIND_DATA}${PATHNAME.DATASETS}`}
-          component={routes[PATHNAME.DATASETS]}
-        />
-        <Route path={PATHNAME.ABOUT} component={routes[PATHNAME.ABOUT]} />
-        <Route
+          exact
           path={PATHNAME.FIND_DATA}
           component={routes[PATHNAME.FIND_DATA]}
         />
         <Route
           exact
+          path={`${PATHNAME.FIND_DATA}${PATHNAME.DATASETS}`}
+          component={routes[PATHNAME.DATASETS]}
+        />
+        <Route exact path={PATHNAME.NEWS} component={routes[PATHNAME.NEWS]} />
+        <Route
+          exact
           path={`${PATHNAME.NEWS}/:newsArticleId`}
           component={routes.NEWS_ARTICLE}
         />
-        <Route path={PATHNAME.NEWS} component={routes[PATHNAME.NEWS]} />
       </Switch>
     </Suspense>
   </BrowserRouter>
