08ce32e972739ee39081235cc864c7c72a75cc39
Øyvind Isaksen <oyvindisaks1@gmail.com>
2021-06-21 13:44:34 +0200

    fix(find-data): connect to cms-page

--- src/entrypoints/main/router/pages/find-data-page/index.tsx
+++ src/entrypoints/main/router/pages/find-data-page/index.tsx
@@ -1,47 +1,89 @@
-import React, { FC, memo } from 'react';
+import React, { FC, memo, useEffect } from 'react';
 import { compose } from 'redux';
 import { Link as RouterLink } from 'react-router-dom';
+
+import withPage, {
+  Props as CmsArticleProps
+} from '../../../../../components/with-cms-page';
 
 import Root from '../../../../../components/root';
 import Container from '../../../../../components/container';
 
-import Translation from '../../../../../components/translation';
-
-import SC from './styled';
 import {
   InfoBox,
   InfoBoxBody,
   InfoBoxTitle
 } from '../../../../../components/info-box';
-import { PATHNAME } from '../../../../../enums';
+import { PARAGRAPH } from '../../../../../enums';
+import { convertToSanitizedHtml } from '../../../../../utils/markdown-converter';
 
-interface Props {}
+import SC from './styled';
 
-const FindDataPage: FC<Props> = () => (
-  <Root>
-    <Container>
-      <SC.Page>
-        <SC.Title>
-          <Translation id='findDataPage.title' />
-        </SC.Title>
-        <SC.Content>
-          <InfoBox
-            as={RouterLink}
-            to={`${PATHNAME.FIND_DATA}${PATHNAME.DATASETS}`}
-          >
-            <InfoBoxTitle>
-              <h2>
-                <Translation id='findDataPage.findData' />
-              </h2>
-            </InfoBoxTitle>
-            <InfoBoxBody>
-              <Translation id='findDataPage.findDataText' />
-            </InfoBoxBody>
-          </InfoBox>
-        </SC.Content>
-      </SC.Page>
-    </Container>
-  </Root>
-);
+const articleId = '8d6ed4d9-f2b9-4d24-9561-595af3876240';
 
-export default compose<FC>(memo)(FindDataPage);
+interface Props extends CmsArticleProps {}
+
+export const renderModule = (module: any) => {
+  switch (module.type) {
+    case PARAGRAPH.INFO_BOX: {
+      const infoBoxUrl = module?.field_link?.uri?.replace('internal:', '');
+      // eslint-disable-next-line no-console
+      console.log('infoUrl', infoBoxUrl);
+      const r = new RegExp('^(?:[a-z]+:)?//', 'i');
+      const isAbsoluteUrl = r.test(infoBoxUrl) ?? false;
+      // eslint-disable-next-line no-console
+      console.log('isAbsoluteUrl', isAbsoluteUrl);
+      return (
+        <InfoBox
+          key={module.id}
+          {...(isAbsoluteUrl
+            ? { href: module.field_link?.uri }
+            : { as: RouterLink, to: infoBoxUrl })}
+        >
+          <InfoBoxTitle>
+            <h2>{module.field_link?.title}</h2>
+          </InfoBoxTitle>
+          <InfoBoxBody>
+            <div
+              // eslint-disable-next-line react/no-danger
+              dangerouslySetInnerHTML={{
+                __html: convertToSanitizedHtml(module.field_body?.processed)
+              }}
+            />
+          </InfoBoxBody>
+        </InfoBox>
+      );
+    }
+    default:
+      return null;
+  }
+};
+
+const FindDataPage: FC<Props> = ({
+  cmsPage,
+  cmsPageActions: { getCmsPageRequested: getCmsPage, resetCmsPage }
+}) => {
+  useEffect(() => {
+    getCmsPage(articleId);
+    return () => {
+      resetCmsPage();
+    };
+  }, []);
+
+  const modules = cmsPage?.field_modules ?? [];
+
+  return (
+    <Root>
+      <Container>
+        <SC.Page>
+          <SC.Title>{cmsPage?.title}</SC.Title>
+          <SC.Content>
+            {modules?.map((module: any) => renderModule(module))}
+          </SC.Content>
+        </SC.Page>
+      </Container>
+    </Root>
+  );
+};
+
+export default compose<FC>(memo, withPage)(FindDataPage);
--- src/entrypoints/main/router/pages/find-data-page/styled.ts
+++ src/entrypoints/main/router/pages/find-data-page/styled.ts
@@ -14,6 +14,7 @@ const Title = styled.h1`
 
 const Content = styled.div`
   display: flex;
+  flex-wrap: wrap;
   justify-content: space-between;
 `;
 
