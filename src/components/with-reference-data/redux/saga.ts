726ea0e81abb648921eeca973805a179b94f915a
Øyvind Isaksen <oyvindisaks1@gmail.com>
2021-06-22 11:38:28 +0200

    feat(with-reference-data): add with-reference-data hoc

---
+++ src/components/with-reference-data/redux/saga.ts
@@ -0,0 +1,40 @@
+import { all, call, put, takeEvery } from 'redux-saga/effects';
+
+import { GET_REFERENCE_DATA_REQUESTED } from './action-types';
+import * as actions from './actions';
+
+import { getReferenceData } from '../../../services/api/reference-data/reference-data';
+
+import type { ReferenceType } from '../../../types';
+
+function* getReferenceDataRequested({
+  payload: { category }
+}: ReturnType<typeof actions.getReferenceDataRequested>) {
+  try {
+    const codes = [
+      'referencetypes',
+      'mediatypes',
+      'linguisticsystem',
+      'apiservicetype'
+    ];
+
+    const data: ReferenceType[] = yield call(
+      getReferenceData,
+      codes.includes(category) ? `codes/${category}` : category
+    );
+
+    if (data) {
+      yield put(actions.getReferenceDataSucceeded(category, data));
+    } else {
+      yield put(actions.getReferenceDataFailed(''));
+    }
+  } catch (e) {
+    yield put(actions.getReferenceDataFailed(e.message));
+  }
+}
+
+export default function* saga() {
+  yield all([
+    takeEvery(GET_REFERENCE_DATA_REQUESTED, getReferenceDataRequested)
+  ]);
+}
---
+++ src/services/api/reference-data/reference-data.ts
@@ -0,0 +1,3 @@
+import { referenceDataGet } from './host';
+
+export const getReferenceData = (path: string) => referenceDataGet(path);
